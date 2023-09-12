cfd403c4afa67ea912365fd5335b60576cc9da35
pdrmenezes <pdrmenezes1@gmail.com>
2023-09-11 22:08:49 -0300

    nlw-ai first commit

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NLW AI",
+  description: "NLW event using OpenAI's API to generate title's and captions",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
