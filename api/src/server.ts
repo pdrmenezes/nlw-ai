3e3b7174b6be4f86949f5c7649aefd69e8480b24
pdrmenezes <pdrmenezes1@gmail.com>
2023-09-12 21:57:23 -0300

    api/feat:completion route + api cors setup

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getPromptsRoute } from "./routes/get-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const api = fastify();
+
+api.register(fastifyCors, {
+  origin: "*",
+});
 
 api.register(getPromptsRoute);
 api.register(uploadVideoRoute);
 api.register(createTranscriptionRoute);
+api.register(generateAICompletionRoute);
 
 api
   .listen({
--- web/src/app/api/video/route.ts
+++ web/src/app/api/video/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST() {
   const prompts = await prisma.prompt.findMany({});
 
   return NextResponse.json({ prompts });
