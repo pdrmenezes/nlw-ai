9680db09ad5da4675ae36a7253e0309afc5cd7a5
pdrmenezes <pdrmenezes1@gmail.com>
2023-09-12 21:17:41 -0300

    api/feat:upload video route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from "fastify";
 import { getPromptsRoute } from "./routes/get-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const api = fastify();
 
 api.register(getPromptsRoute);
+api.register(uploadVideoRoute);
 
 api
   .listen({
